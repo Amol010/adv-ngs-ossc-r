8109b419e251eba68f85c36d893b62e53ee62c78
amol010 <amolmishra50@gmail.com>
2023-04-20 19:40:50 +0530

    ngs 2

--- ngs-class-02.R
+++ ngs-class-02.R
@@ -2,5 +2,47 @@
 
 ## SMALL HEADER
 
+```{R}
+# vectors
+# character vector, numeric vector, logical vector & factor
 
+# single element
+char_vec <- 'hello'
 
+# multiple elements
+char_vec_many = c('a','c','h')
+
+# numeric vector
+num_vec <- c(1, 4.5, 8, 10)
+
+# charcter vec
+xg = c(2, "a", 10)
+
+#logical vector
+logical_vec <- char_vec_many == 'c'
+
+char_vec <- 'hello'
+ls()
+char_vec_many = c('a','c','h')
+ls()
+'hello'
+1
+1+1
+strsplit(x=char_vec, split="")
+num_vec <- c(1, 4.5, 8, 10)
+num_vec
+xg = c(2, "a", 10)
+xg
+char_vec_many
+char_vec_many == 'c'
+logical_vec <- char_vec_many == 'c'
+logical_vec
+logical_vec [1]
+logical_vec [2]
+logical_vec [3]
+char_vec_many[logical_vec]
+!FALSE
+char_vec_many[!logical_vec]
+!=
+1 != 2
+```
